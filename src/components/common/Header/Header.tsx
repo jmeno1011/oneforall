5969a8a23f227a1c5e428c5811e2c80c0103ca44
jmeno1011 <jwhltn8282@gmail.com>
2024-02-13 15:52:37 +0900

    add profile button

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,13 +1,42 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../../redux/features/auth";
+import Profile from "../../../assets/header/person.svg?react";
+import { RootState } from "../../../redux/store";
+import { useEffect, useRef, useState } from "react";
 
 export default function Header() {
   const dispatch = useDispatch();
+  const { userID } = useSelector((state: RootState) => state.auth);
+  const [openProfile, setOpenProfile] = useState<boolean>(false);
+  const profileRef = useRef<HTMLDivElement>(null);
+
+  const onClickOpenProfile = () => {
+    setOpenProfile(!openProfile);
+  };
+
   const onClickSignout = () => {
     dispatch(logout());
   };
+
+  const handleClickOutside = (e: MouseEvent) => {
+    const target = e.target as HTMLElement;
+    if (
+      profileRef.current &&
+      target &&
+      target.contains(profileRef.current) &&
+      !profileRef.current.contains(target)
+    ) {
+      setOpenProfile(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", handleClickOutside);
+    return () => window.removeEventListener("click", handleClickOutside);
+  }, [profileRef]);
+
   return (
     <header className={styles.container}>
       <div>
@@ -15,10 +44,27 @@ export default function Header() {
           <span>Home</span>
         </Link>
       </div>
-      <div>
+      <div className={styles.headerButton}>
+        <button className={styles.profileButton} onClick={onClickOpenProfile}>
+          <Profile />
+        </button>
         <button onClick={onClickSignout} className={styles.signoutButton}>
           <span>Sign out</span>
         </button>
+        {openProfile ? (
+          <div className={styles.smallProfile} ref={profileRef}>
+            <h5>
+              아이디: <span>{userID}</span>
+            </h5>
+            <div className={styles.smallProfileBottom}>
+              <button>
+                <Link to={"/profile"}>
+                  <span>프로필</span>
+                </Link>
+              </button>
+            </div>
+          </div>
+        ) : null}
       </div>
     </header>
   );
