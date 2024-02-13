9cd08bc09ddaf009798f58eb44f625aff65182d3
jmeno1011 <jwhltn8282@gmail.com>
2024-02-13 12:01:17 +0900

    add join

--- src/pages/Join/Join.tsx
+++ src/pages/Join/Join.tsx
@@ -1,7 +1,43 @@
+import { Link } from "react-router-dom";
+import AuthLayout from "../../components/common/AuthLayout/AuthLayout";
 import styles from "./Join.module.css";
 
 export default function Join() {
   return (
-    <div className={styles.container}>Join</div>
-  )
+    <AuthLayout>
+      <header className={styles.header}>
+        <h3>회원가입</h3>
+      </header>
+      <form className={styles.form}>
+        <div className={styles.inputRow}>
+          <label htmlFor="id">아이디</label>
+          <input type="text" name="" id="id" required />
+        </div>
+        <div className={styles.inputRow}>
+          <label htmlFor="pw">비밀번호</label>
+          <input type="text" name="" id="pw" required />
+        </div>
+        <div className={styles.inputRow}>
+          <label htmlFor="pw2">비밀번호 확인</label>
+          <input type="text" name="" id="pw2" required />
+        </div>
+        <div className={styles.inputRow}>
+          <label htmlFor="name">이름</label>
+          <input type="text" name="" id="name" required />
+        </div>
+        <div className={styles.inputRow}>
+          <label htmlFor="email">이메일</label>
+          <input type="email" name="" id="email" required />
+        </div>
+        <button>가입하기</button>
+        <div className={styles.formBottom}>
+          <button>
+            <Link to={"/login"}>
+              <span>로그인</span>
+            </Link>
+          </button>
+        </div>
+      </form>
+    </AuthLayout>
+  );
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -9,8 +9,8 @@ export default function Login() {
         <h3>로그인</h3>
       </header>
       <form className={styles.form}>
-        <input type="text" name="" id="" />
-        <input type="password" name="" id="" />
+        <input type="text" placeholder="아이디" />
+        <input type="password" placeholder="비밀번호" />
         <button>로그인</button>
         <div className={styles.formBottom}>
           <button>
