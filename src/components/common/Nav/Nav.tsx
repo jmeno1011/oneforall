785b1def2d50050bce238a8a966a2a48fbd3c429
jmeno1011 <jwhltn8282@gmail.com>
2024-02-20 12:15:34 +0900

    add css in Nav

--- src/components/common/Nav/Nav.tsx
+++ src/components/common/Nav/Nav.tsx
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import Home from "../../../assets/Nav/home.svg?react";
 import styles from "./Nav.module.css";
 import { useState } from "react";
+import classNames from "classnames/bind";
+const cn = classNames.bind(styles);
 
 export default function Nav() {
   const [accordionNav, setAccordionNav] = useState([
@@ -20,32 +23,45 @@ export default function Nav() {
       children: [],
     },
   ]);
-  const selectNavTitle = (title: string)=>{
-    const existTitle = accordionNav.filter(t => t.title === title);
-    if(existTitle.length === 0){
-      setAccordionNav(accordionNav.map(v => v.title === title ? ({
-        ...v,
-        opened: false
-      }): v))
-    }
-  }
+
+  const {pathname} = useLocation();
+  const selectNavTitle = (title: string) => {
+    setAccordionNav(
+      accordionNav.map((v) =>
+        v.title === title
+          ? {
+              ...v,
+              opened: !v.opened,
+            }
+          : v
+      )
+    );
+  };
   return (
     <div className={styles.container}>
       <nav>
         {accordionNav.map((nav) => (
           <div className={styles.navWrapper}>
-            <h5 className={styles.navTitle} onClick={()=>selectNavTitle(nav.title)}>{nav.title}</h5>
-            <ul>
-              {nav.children.map((child) => (
-                <li>
-                  <Link to={child.link}>
-                    <div className={styles.childrenTitle}>
-                      <span>{child.name}</span>
-                    </div>
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <h5
+              className={styles.navTitle}
+              onClick={() => selectNavTitle(nav.title)}
+            >
+              {nav.title}
+            </h5>
+            {nav.opened ? (
+              <ul>
+                {nav.children.map((child) => (
+                  <li>
+                    <Link to={child.link}>
+                      <div className={cn('childrenTitle', child.link === pathname && 'childrenSelected')}>
+                        <Home />
+                        <span>{child.name}</span>
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
           </div>
         ))}
       </nav>
